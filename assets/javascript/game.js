8b07cb218f2997336dde1ff0fc7810bb50ce5830
Greg Quintana <quintana.greg444@gmail.com>
2019-07-08 00:00:59 -0600

    added new index html style.css and js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,26 @@
-var wordBank = ["javascript","python","jquery","mysql", "react"];
-var randomWord  
-function getRandomWord() {
- var index =Math.floor(Math.random() * wordBank.length);
- randomWord=wordBank[index];
 
-}
-getRandomWord()
-console.log($("#randomWord"))
-$("#randomWord").html(randomWord)
+const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var randomLetter
+var letterGuess;
 
- $(document).on("keypress", function (event){
-  console.log(String.fromCharCode(event.keyCode)
-  ) })
+var getLetter = letters(Math.random()*letters.length)];
+
+document.body.innerHTML = getLetter;
+
+//Need to create events for buttton click start game
+  // Need to index a random letter from Array 
+  //need to store  key press from user 
+  //check to see if key press matches word guess by serching Array 
+  //Write if  else statements for condition true/false 
+  // write function to keep track of counter for tries display back to user on guess 
+  // end game if over 9 guess 
+  // if guess word alert Congratulations 
+  //loop to start 
+
+  
+
+
+
+
+  
 
